6f601365f16c35d97bd2cda6e3e175651b71153d
chaineze7 <chachanam24@gmail.com>
2025-10-01 16:40:59 +0200

    Add files via upload

---
+++ chaineze-api-eatsmart/index.php
@@ -0,0 +1,60 @@
+<?php
+
+require_once "./controllers/ArticleController.php";
+require_once "./controllers/CategorieController.php";
+require_once "./controllers/CommandeController.php";
+
+$articleController = new ArticleController();
+$categorieController = new CategorieController();
+$commandeController = new CommandeController();
+
+
+// Vérifie si le paramètre "page" est vide ou non présent dans l'URL
+if (empty($_GET["page"])) {
+    // Si le paramètre est vide, on affiche un message d'erreur
+    echo "La page n'existe pas";
+} else {
+    // Sinon, on récupère la valeur du paramètre "page"
+    // Par exemple, si l’URL est : index.php?page=chauffeurs/3
+    // Alors $_GET["page"] vaut "chauffeurs/3"
+    
+    // On découpe cette chaîne en segments, en séparant sur le caractère "/"
+    // Cela donne un tableau, ex : ["chauffeurs", "3"]
+    $url = explode("/", $_GET['page']);
+    
+    // Affiche le contenu du tableau pour vérifier comment l’URL est interprétée
+    //print_r($url);
+
+    // On teste le premier segment pour déterminer la ressource demandée
+    switch($url[0]) {
+        case "articles" : 
+            // Si un second segment est présent (ex: un ID), on l’utilise
+            if (isset($url[1])) {
+                // Exemple : /articles/3 → affiche les infos de l'article 3
+                echo "Afficher les informations de l'article : ". $url[1];
+            } else {
+                // Sinon, on affiche tous les articles
+                echo  $articleController->getAllArticles();
+            }
+            break;
+        case "categories":
+            if (isset($url[1])) {
+                echo "Afficher les informations de la catégorie : ". $url[1];
+            } else {
+                echo $categorieController->getAllCategories();
+            }
+            break;
+
+        case "commandes":
+            if (isset($url[1])) {
+                echo "Afficher les informations de la commande : ". $url[1];
+            } else {
+                echo $commandeController->getAllCommandes();
+            }
+            break;
+        
+        // Si la ressource n'existe pas, on renvoie un message d’erreur
+        default :
+            echo "La page n'existe pas";
+    }
+}
