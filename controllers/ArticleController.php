345c831ca355c5986c767cd5283add7828b91d55
chaineze7 <chachanam24@gmail.com>
2025-10-10 00:27:22 +0200

    Ajout de fichiers

---
+++ controllers/ArticleController.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once "models/ArticleModel.php";
+
+class ArticleController
+{
+    private $model;
+
+    public function __construct()
+    {
+        $this->model = new ArticleModel();
+    }
+
+    public function getAllArticles()
+    {
+        $articles = $this->model->getDBAllArticles();
+        echo json_encode($articles);
+    }
+}
+//$articleController = new ArticleController();
+//$articleController->getAllArticles();
