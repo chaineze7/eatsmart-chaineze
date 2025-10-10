a986cd0f6f8c34c0bb621328d7f90358c2ff69a8
Chaineze Said <chachanam24@gmail.com>
2025-10-10 18:15:52 +0200

    Ajout de fichiers

---
+++ controllers/CategorieController.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once "models/CategorieModel.php";
+
+class CategorieController
+{
+    private $model;
+
+    public function __construct()
+    {
+        $this->model = new CategorieModel();
+    }
+
+    public function getAllCategories()
+    {
+        $categories = $this->model->getDBAllCategories();
+        echo json_encode($categories);
+    }
+}
+//$categorieController = new CategorieController();
+//$categorieController->getAllCategories();
---
+++ models/CategorieModel.php
@@ -0,0 +1,24 @@
+<?php
+
+class CategorieModel
+{
+    private $pdo;
+
+    public function __construct()
+    {
+        try {
+            $this->pdo = new PDO("mysql:host=localhost;dbname=eatsmart_bdd_bruno;charset=utf8", "root", "");
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die("Erreur de connexion à la base de données : " . $e->getMessage());
+        }
+    }
+
+    public function  getDBAllCategories()
+    {
+        $stmt = $this->pdo->query("SELECT * FROM categorie");
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
+//$categorieModel = new CategorieModel();
+//print_r($categorieModel->getDBAllCategories());
